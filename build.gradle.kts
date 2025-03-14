0c975b7463ab97531d22b86037d7621589d3712d
JChurchley91 <joseph.churchley@outlook.com>
2025-03-14 12:20:14 +0300

    basic logging added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 val ktorVersion: String by project
+val logBackVersion: String by project
 
 plugins {
     kotlin("jvm") version "2.1.10"
@@ -15,6 +16,8 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logBackVersion")
+    implementation("org.slf4j:slf4j-api:1.7.32")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,15 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import org.slf4j.LoggerFactory
+
+val logger: org.slf4j.Logger = LoggerFactory.getLogger("news-fetcher")
 
 suspend fun main() {
+    logger.info("Sending request to ktor.io")
     val client = HttpClient(CIO)
     val response: HttpResponse = client.get("https://ktor.io/")
     println(response.status)
+    logger.info("Received response with status: ${response.status}")
     client.close()
 }
