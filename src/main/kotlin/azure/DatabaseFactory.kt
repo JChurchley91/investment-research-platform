fe027a9488495390754ceb9e0001ea6ed26edc87
JChurchley91 <joseph.churchley@outlook.com>
2025-03-28 14:54:31 +0300

    better auth added

--- src/main/kotlin/azure/DatabaseFactory.kt
+++ src/main/kotlin/azure/DatabaseFactory.kt
@@ -1,30 +1,41 @@
 package azure
 
+import io.github.cdimascio.dotenv.dotenv
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.util.Properties
 
 val logger: Logger = LoggerFactory.getLogger("DatabaseFactory")
+val dotenv = dotenv()
+
+object TestTable : Table() {
+    val id = integer("id").autoIncrement()
+    override val primaryKey = PrimaryKey(id)
+}
 
 object DatabaseFactory {
     fun init(): Boolean {
-        val props =
-            Properties().apply {
-                load(Thread.currentThread().contextClassLoader.getResourceAsStream("database.properties"))
+        return try {
+            val db =
+                Database.connect(
+                    url = dotenv["DB_URL"],
+                    driver = "org.postgresql.Driver",
+                    user = dotenv["DB_USER"],
+                    password = dotenv["DB_PASSWORD"],
+                )
+
+            // Execute a simple query to check the connection
+            transaction(db) {
+                SchemaUtils.createMissingTablesAndColumns(TestTable)
+                logger.info("Database connection established successfully.")
             }
-        try {
-            Database.connect(
-                url = props.getProperty("db.url"),
-                driver = "org.postgresql.Driver",
-                user = props.getProperty("db.user"),
-                password = props.getProperty("db.password"),
-            )
-            logger.info("Database connection established successfully.")
             return true
         } catch (exception: Exception) {
-            logger.info("Failed to connect to the database: ${exception.message}")
-            return false
+            logger.error("Failed to connect to the database: ${exception.message}")
+            false
         }
     }
 }
